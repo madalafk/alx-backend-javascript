bd80f7f457c291918b23a29d95ffeabbf048646b
madalafk <fmadala@hotmail.com>
2023-11-30 12:41:49 -0800

    5-building.js

---
+++ 0x02-ES6_classes/5-main.js
@@ -0,0 +1,14 @@
+bob@dylan:~$ cat 5-main.js
+import Building from './5-building.js';
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+    new TestBuilding(200)
+}
+catch(err) {
+    console.log(err);
+}
