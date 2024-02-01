19f906540b001fa0990828fdd50b788770e8fa6c
madalafk <fmadala@hotmail.com>
2024-02-01 19:15:44 +0000

    3-payment.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,24 +1,24 @@
 class Utils {
     static calculateNumber(type, a, b) {
-        const NumA = Math.round(a);
-        const NumB = Math.round(b);
-        let NumC = 0;
-        switch (type) {
-            case 'SUM':
-                NumC = NumA + NumB;
-                break;
-            case 'SUBTRACT':
-                NumC = NumA - NumB;
-                break;
-            case 'DIVIDE':
-                if (NumB === 0) {
-                    c = "Error";
-                } else {
-                    NumC = NumA / NumB;
-                }
-                break;
-        }
-        return NumC;
+	const NumA = Math.round(a);
+	const NumB = Math.round(b);
+	let NumC = 0;
+	switch (type) {
+	case 'SUM':
+            NumC = NumA + NumB;
+            break;
+	case 'SUBTRACT':
+            NumC = NumA - NumB;
+            break;
+	case 'DIVIDE':
+	    if (NumB === 0) {
+		c = "Error";
+	    } else {
+		NumC = NumA / NumB;
+	    }
+	    break;
+	}
+	return NumC;
     }
 }
 
